14ba43daa4d0cd43354b18b881c4ef2362983d89
Wade <wade.darsow@gmail.com>
2020-05-30 23:17:06 -0700

    first exercise

---
+++ src/components/HelloDiv.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const name = "Wade"
+const HelloDiv = () => 
+    <div>
+        <h1>Hello! My name is {name} </h1>
+            <ul>
+                <li>Coffee</li>
+                <li>Tea</li>
+                <li>Sparkling Water</li>
+            </ul>
+    </div>;
+
+export default HelloDiv;
